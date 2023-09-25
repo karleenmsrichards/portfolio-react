edd05e14e406b402d2eaa658968c536ce2cf55ed
Karleen Richards <karleenmsrichards@gmail.com>
2023-09-25 23:08:49 +0100

    adjuct media queries

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -29,8 +29,9 @@ export const Projects = () => {
         projectSiteLink="https://bwc-medical-app.netlify.app"
         projectGithubLink="https://github.com/Gayle-Thompson-Igwebike/medical-app"
         projectRole={"FULL STACK DEVELOPER"}
+        projectTechStack="Custom CSS, Javascript, React, Node, Express, RESTful API, API Controller, AWS DynamoDB, AWS S3 Bucket."
         projectSummary={
-          "This is a Group Project that brings together medical professionals in various fields of medicine. It is specifically targeted at common diseases that people of african descent face. Patients login to their account before accessing an appointmenr for medical advice."
+          "This is a Team Project that brings together medical professionals in various fields of medicine with the integration of Cloud services. This app specifically targets common diseases faced by people of african descent. Patients login to their account before accessing an appointment for medical advice."
         }
       />
       <ProjectSectionB
@@ -39,8 +40,9 @@ export const Projects = () => {
         projectSiteLink="https://weather-app-karleen.onrender.com"
         projectGithubLink="https://github.com/karleenmsrichards/weather-app"
         projectRole={"FRONTEND DEVELOPER"}
+        projectTechStack="Custom CSS, Javascript, React, RESTful API."
         projectSummary={
-          "Weather App is a front end website which displays the current weather and conditions. Users can also check the weather for any city in the world. A restful API was used to access the weather data."
+          "Weather App is a front end website which displays the current weather and conditions or check the weather for any city in the world. A restful API was used to access the weather data. I also came up with an exciting solution for achieving animated weather icons for this app though they were not provided by the API."
         }
       />
       <ProjectSectionA
@@ -71,8 +73,9 @@ export const Projects = () => {
         projectSiteLink="https://cyf-karleenmsrichards-tv.netlify.app/"
         projectGithubLink="https://github.com/karleenmsrichards/tv-show-dom-project"
         projectRole={"FRONTEND DEVELOPER"}
+        projectTechStack="HTML5, Custom CSS, JavaScript, RESTful API."
         projectSummary={
-          "This is a website developed using Vanilla Javascript. My creativity and passion for the tech industry is visible in this TV Show Project. Calls to TvMaze's restful API is are made so that shows and movies are displayed. A few other stretch goal features were also accomplished. Please watch!"
+          "This is a website developed using Vanilla Javascript. My creativity and passion for the tech industry is visible in this TV Show Project. Calls to TvMaze's RESTful API are made so that shows and movies are displayed. A few stretch goal features were also accomplished."
         }
       />
       <ProjectSectionA
@@ -81,6 +84,7 @@ export const Projects = () => {
         projectSiteLink="https://empowered.netlify.app"
         projectGithubLink="https://github.com/bazmurphy/buddy-group-project"
         projectRole={"UX/UI"}
+        projectTechStack="HTML5, Custom CSS, JavaScript, Figma"
         projectSummary={
           "This website is a group effort that helps users lower the cost of their utility bills. In the process of developing this website, I communicated and collaborated well with all team members and came up with various ways to deal with difference of opinions. For instance, I listened to my teammates and weighed the pros and cons before making final decisions."
         }
@@ -90,6 +94,7 @@ export const Projects = () => {
         projectTitle={"CYF Hotel"}
         projectGithubLink="https://github.com/karleenmsrichards/cyf-hotel-react"
         projectRole={"FULL STACK DEVELOPER"}
+        projectTechStack="JavaScript, React, Node, Express, RESTful API, Custom CSS."
         projectSummary={""}
       />
       <ProjectSectionA
@@ -98,6 +103,7 @@ export const Projects = () => {
         projectSiteLink="https://karleenmsrichards-quotes-site-ne4r-qf3fdamp4-karleenmsrichards.vercel.app/"
         projectGithubLink="https://github.com/karleenmsrichards/karleenmsrichards-quotes-site"
         projectRole={"FULL STACK DEVELOPER"}
+        projectTechStack="JavaScript, React, Node, Express, RESTful API, Custom CSS."
         projectSummary={""}
       />
       <ProjectSectionB
@@ -105,6 +111,7 @@ export const Projects = () => {
         projectTitle={"Juice Bar"}
         projectRole={"FRONTEND DEVELOPER"}
         projectSiteLink=""
+        projectTechStack="HTML5, Responsive Grid, Custom CSS."
         projectGithubLink="https://github.com/karleenmsrichards/responsive-grid-project"
         projectSummary={
           "This is a front-end website where I used the CSS grid property and media queries to change the layout of the page depending on the width of the viewport. I also used the CSS position property and rotate to complete my website."
@@ -116,6 +123,7 @@ export const Projects = () => {
         projectSiteLink="https://chat-with-me-client.onrender.com"
         projectGithubLink="https://github.com/karleenmsrichards/chat-with-me"
         projectRole={"FULL STACK DEVELOPER"}
+        projectTechStack="JavaScript, React, Node, Express, Prisma, Google OAuth 2.0, Custom CSS."
         projectSummary={
           "Chat-with-me is a messaging app that uses Google Auth. This code using an ORM - Prisma to access the site's database. For styling I have utilized TailwindCSS."
         }
