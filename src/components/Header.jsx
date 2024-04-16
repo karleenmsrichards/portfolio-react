5efff1fd1101dee6bd75b0550fd55c27abee6c62
karleenmsrichards <karleenmsrichards@gmail.com>
2024-04-16 07:51:18 +0100

    add link to cv to cv nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,58 +1,79 @@
 import { useState } from "react";
 import "../components/Header.css";
 import NavBar from "../components/NavBar";
-import githubIcon from "../images/github-icon.png"
-import linkedInIcon from "../images/linkedin-icon.png"
+import githubIcon from "../images/github-icon.png";
+import linkedInIcon from "../images/linkedin-icon.png";
 
-export const Header = ({setShowPopup}) => {
+
+export const Header = ({ setShowPopup }) => {
   const [showMenu, setShowMenu] = useState(false);
   const toggleMenu = () => setShowMenu((s) => !s);
 
   const handleClose = () => setShowMenu(false);
 
-    return (
-      <header>
-        <div className="header-logo-name-wrapper">
-          <a href="/" className="header-logo-name-text">
+  return (
+    <header>
+      <div className="header-logo-name-wrapper">
+        <a href="/" className="header-logo-name-text">
           Karleen Richards
-          </a>
-        </div>
+        </a>
+      </div>
 
-        <div id="cv">
-            <p className="nav-btn cv-btn" onClick={() => setShowPopup(true)}>CV</p>
-        </div>
+      <div id="cv">
+        <a className="nav-btn cv-btn" href="https://drive.google.com/file/d/1n7f208HRcLGH9Kh2hBOh_2j9iH-n1_nm/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+          CV
+        </a>
+      </div>
 
-        <div id="projects-wrapper">
-          <a href="/#javascript-projects" className="nav-btn">PROJECTS</a>
-        </div>
+      <div id="projects-wrapper">
+        <a href="/#javascript-projects" className="nav-btn">
+          PROJECTS
+        </a>
+      </div>
 
-        <div id="skills-wrapper">
-          <a href="/#skills" className="nav-btn">
-            SKILLS
-          </a>
-        </div>
+      <div id="skills-wrapper">
+        <a href="/#skills" className="nav-btn">
+          SKILLS
+        </a>
+      </div>
 
-        <div id="tech-stack-wrapper">
-          <a href="/#tech-stack" className="nav-btn">
-            TECH STACK
-          </a>
-        </div>
+      <div id="tech-stack-wrapper">
+        <a href="/#tech-stack" className="nav-btn">
+          TECH STACK
+        </a>
+      </div>
 
-        <div id="contact">
-            <p className="nav-btn" onClick={() => setShowPopup(true)}>CONTACT</p>
-        </div>
+      <div id="contact">
+        <p className="nav-btn" onClick={() => setShowPopup(true)}>
+          CONTACT
+        </p>
+      </div>
 
-        <div id="social-media-links">
-        <a className="nav-btn" href="https://github.com/your_username" target="_blank" rel="noopener noreferrer">
+      <div id="social-media-links">
+        <a
+          className="nav-btn"
+          href="https://github.com/your_username"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img className="social-media-icon" src={githubIcon} alt="GitHub" />
         </a>
-      
-        <a className="nav-btn" href="https://linkedin.com/in/your_profile" target="_blank" rel="noopener noreferrer">
-           <img  className="social-media-icon" src={linkedInIcon} alt="LinkedIn" />
+
+        <a
+          className="nav-btn"
+          href="https://linkedin.com/in/your_profile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="social-media-icon"
+            src={linkedInIcon}
+            alt="LinkedIn"
+          />
         </a>
-        </div>
-        
-        {showMenu && <NavBar showMenu={showMenu} handleClose={handleClose} />}
-      </header>
+      </div>
+
+      {showMenu && <NavBar showMenu={showMenu} handleClose={handleClose} />}
+    </header>
   );
 };
